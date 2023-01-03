b7275343459772abc0aee1090154c78e6f9303e9
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-01-02 18:01:00 -0800

    done

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,32 @@
+class RecipesController < ApplicationController
+    before_action :authorize
+
+    def index
+        recipes = Recipe.all
+        if session[:user_id]
+            render json: recipes, status: :ok
+        else
+            render json: { errors: ["Log In"] }, status: :unauthorized
+        end
+    end
+
+    def create
+        @user = User.find_by(id: session[:user_id])
+        recipe = Recipe.create(recipe_params.merge({user_id: @user.id}))
+        if recipe.valid?
+            render json: recipe, status: :created
+        else
+            render json: { errors: recipe.errors.full_messages }, status: :unprocessable_entity
+        end 
+    end
+
+    private
+    
+    def authorize
+        return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+    end
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+end
